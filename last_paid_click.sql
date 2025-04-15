18b3add5007545b0c32e3bbe2b35363fd0ba6f48
CodingKarp <alexbishkek67@gmail.com>
2025-04-15 09:22:38 +0300

    Update last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,41 +1,33 @@
-with last_visit as (
+with last_visits as (
     select
         l.visitor_id,
-        s.visit_date,
-        s.source as utm_source,
-        s.medium as utm_medium,
-        s.campaign as utm_campaign,
-        l.lead_id,
-        l.created_at,
-        l.amount,
-        l.closing_reason,
-        l.status_id,
-        row_number()
-        over (partition by l.visitor_id order by s.visit_date desc)
-        as rn
+        MAX(s.visit_date) as last_visit
     from sessions as s
-    left join
-        leads as l
+    left join leads as l
         on s.visitor_id = l.visitor_id and s.visit_date <= l.created_at
     where s.medium not in ('organic')
+    group by 1
 )
-select
-    visitor_id,
-    visit_date,
-    utm_source,
-    utm_medium,
-    utm_campaign,
-    lead_id,
-    created_at,
-    amount,
-    closing_reason,
-    status_id
-from last_visit lv
-where rn = '1'
+
+select distinct
+    s.visitor_id,
+    lv.last_visit as visit_date,
+    s.source as utm_source,
+    s.medium as utm_medium,
+    s.campaign as utm_campaign,
+    l.created_at,
+    l.amount,
+    l.closing_reason,
+    l.status_id
+from last_visits as lv
+left join sessions as s
+    on lv.visitor_id = s.visitor_id and lv.last_visit = s.visit_date
+left join leads as l
+    on s.visitor_id = l.visitor_id
 order by
     amount desc nulls last,
     visit_date asc,
     utm_source asc,
-    utm_medium asc,
-    utm_campaign asc
+    utm_campaign asc,
+    utm_medium asc
 limit 10;
