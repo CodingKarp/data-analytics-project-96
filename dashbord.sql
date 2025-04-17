38cbac73ad34f0c620846f142f14a1861148d960
CodingKarp <alexbishkek67@gmail.com>
2025-04-17 16:09:23 +0300

    Update dashbord.sql

--- dashbord.sql
+++ dashbord.sql
@@ -127,6 +127,128 @@ order by 1, 2;
 -- количество лидов 
 select count(lead_id) as lead_count from leads;
 
+-- конверсия визит -> лид -> покупка
+with last_visits as (
+    select
+        s.visitor_id,
+        max(s.visit_date) as last_visit
+    from sessions as s
+    where s.medium not in ('organic')
+    group by 1
+),
+
+last_paid_click as (
+    select
+        s.visitor_id,
+        lv.last_visit as visit_date,
+        s.source as utm_source,
+        s.medium as utm_medium,
+        s.campaign as utm_campaign,
+        l.lead_id,
+        l.created_at,
+        l.amount,
+        l.closing_reason,
+        l.status_id
+    from sessions as s
+    inner join last_visits as lv
+        on s.visitor_id = lv.visitor_id and s.visit_date = lv.last_visit
+    left join leads as l
+        on s.visitor_id = l.visitor_id and s.visit_date <= l.created_at
+    order by
+        l.amount desc nulls last,
+        s.visit_date asc,
+        s.source asc,
+        s.campaign asc,
+        s.medium asc
+),
+
+visit_lead_purchase as (
+    select
+        count(distinct visitor_id) as visitors_count,
+        count(distinct lead_id) as leads_count,
+        sum(
+            case
+                when
+                    closing_reason = 'успешно реализовано' or status_id = 142
+                    then 1
+                else 0
+            end
+        ) as purchases_count
+    from last_paid_click
+)
+
+select
+    round(leads_count * 100.00 / visitors_count, 2) as visit_to_lead,
+    round(purchases_count * 100.00 / leads_count, 2) as lead_to_purchase
+from visit_lead_purchase;
+
+-- расходы по каждому каналу по неделям 
+select
+    'vk' as source,
+    to_char(campaign_date, 'w'),
+    sum(daily_spent),
+    sum(revenue)
+from vk_ads
+group by 1, 2
+
+union
+
+select
+    'ya' as source,
+    to_char(campaign_date, 'w'),
+    sum(daily_spent)
+from ya_ads
+group by 1, 2
+order by 2;
+
+-- окуппаемость канналов 
+with cost as (
+select
+    'vk' as source,
+    sum(daily_spent) as total_spent
+from vk_ads
+group by 1
+
+union
+
+select
+    'ya' as source,
+    sum(daily_spent) as total_spent
+from ya_ads
+group by 1
+), 
+revenue as ( 
+select 
+	'vk' as source, 
+	sum(l.amount) as total_revenue
+from leads as l 
+inner join sessions as s 
+	on l.visitor_id = s.visitor_id and source = 'vk'
+	
+union 
+
+select 
+	'ya' as source, 
+	sum(l.amount) as total_revenue
+from leads as l 
+inner join sessions s 
+	on l.visitor_id = s.visitor_id and source = 'yandex'
+)
+select 
+	c.source,
+	r.total_revenue as total_revenue,
+	c.total_spent as total_spent, 
+	r.total_revenue - c.total_spent as profit
+from cost as c
+inner join revenue as r using(source); 
+
+
+
+
+
+
+
+
 
 
 
