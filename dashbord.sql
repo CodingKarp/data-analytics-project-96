48dca4185d2d3fb699a19e54551fcfcfb3bf5424
CodingKarp <alexbishkek67@gmail.com>
2025-05-05 11:24:42 +0300

    Update dashbord.sql

--- dashbord.sql
+++ dashbord.sql
@@ -140,7 +140,7 @@ select
     to_char(campaign_date, 'w') as week_num,
     sum(daily_spent) as total_spent
 from vk_ads
-group by 1, 2
+group by source, to_char(campaign_date, 'w') 
 
 union
 
@@ -149,8 +149,8 @@ select
     to_char(campaign_date, 'w') as week_num,
     sum(daily_spent) as total_spent
 from ya_ads
-group by 1, 2
-order by 2;
+group by source, to_char(campaign_date, 'w') 
+order by to_char(campaign_date, 'w');
 
 -- окуппаемость каналов 
 with cost as (
@@ -158,7 +158,7 @@ with cost as (
         'vk' as source,
         sum(daily_spent) as total_spent
     from vk_ads
-    group by 1
+    group by source
 
     union
 
@@ -166,7 +166,7 @@ with cost as (
         'ya' as source,
         sum(daily_spent) as total_spent
     from ya_ads
-    group by 1
+    group by source
 ),
 
 revenue as (
@@ -197,108 +197,108 @@ inner join revenue as r on c.source = r.source;
 
 -- основные метрики 
 with last_visits as (
- select
-    s.visitor_id,
-    max(s.visit_date) as last_visit
-from sessions as s
-where s.medium not in ('organic')
-group by 1
+    select
+        s.visitor_id,
+        max(s.visit_date) as last_visit
+    from sessions as s
+    where s.medium not in ('organic')
+    group by s.visitor_id
 
 ),
 
 last_paid_click as (
-   select
-    s.visitor_id,
-    lv.last_visit as visit_date,
-    s.source as utm_source,
-    s.medium as utm_medium,
-    s.campaign as utm_campaign,
-    l.lead_id,
-    l.created_at,
-    l.amount,
-    l.closing_reason,
-    l.status_id
-from sessions as s
-inner join last_visits as lv
-    on s.visitor_id = lv.visitor_id and s.visit_date = lv.last_visit
-left join leads as l
-    on s.visitor_id = l.visitor_id and s.visit_date <= l.created_at
-order by
-    l.amount desc nulls last,
-    s.visit_date asc,
-    s.source asc,
-    s.campaign asc,
-    s.medium asc
+    select
+        s.visitor_id,
+        lv.last_visit as visit_date,
+        s.source as utm_source,
+        s.medium as utm_medium,
+        s.campaign as utm_campaign,
+        l.lead_id,
+        l.created_at,
+        l.amount,
+        l.closing_reason,
+        l.status_id
+    from sessions as s
+    inner join last_visits as lv
+        on s.visitor_id = lv.visitor_id and s.visit_date = lv.last_visit
+    left join leads as l
+        on s.visitor_id = l.visitor_id and s.visit_date <= l.created_at
+    order by
+        l.amount desc nulls last,
+        s.visit_date asc,
+        s.source asc,
+        s.campaign asc,
+        s.medium asc
 ),
 
 aggregation as (
-  select
-    visit_date::date,
-    utm_source,
-    utm_medium,
-    utm_campaign,
-    count(visitor_id) as visitors_count,
-    count(lead_id) as leads_count,
-    sum(
-        case
-            when
-                closing_reason = 'Успешно реализовано' or status_id = 142
-                then 1
-            else 0
-        end
-    ) as purchases_count,
-    sum(amount) as revenue
-from last_paid_click
-group by
-    visit_date::date,
-    utm_source,
-    utm_medium,
-    utm_campaign
+    select
+        visit_date::date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        count(visitor_id) as visitors_count,
+        count(lead_id) as leads_count,
+        sum(
+            case
+                when
+                    closing_reason = 'Успешно реализовано' or status_id = 142
+                    then 1
+                else 0
+            end
+        ) as purchases_count,
+        sum(amount) as revenue
+    from last_paid_click
+    group by
+        visit_date::date,
+        utm_source,
+        utm_medium,
+        utm_campaign
 ),
 
 ad_costs as (
-   select
-    utm_source,
-    utm_medium,
-    utm_campaign,
-    campaign_date,
-    sum(daily_spent) as total_cost
-from vk_ads
-group by utm_source, utm_medium, utm_campaign, campaign_date
-union all
-select
-    utm_source,
-    utm_medium,
-    utm_campaign,
-    campaign_date,
-    sum(daily_spent) as total_cost
-from ya_ads
-group by utm_source, utm_medium, utm_campaign, campaign_date
+    select
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        campaign_date,
+        sum(daily_spent) as total_cost
+    from vk_ads
+    group by utm_source, utm_medium, utm_campaign, campaign_date
+    union all
+    select
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        campaign_date,
+        sum(daily_spent) as total_cost
+    from ya_ads
+    group by utm_source, utm_medium, utm_campaign, campaign_date
 ),
 
 aggregate_last_paid_click as (
-   select
-    a.visit_date,
-    a.utm_source,
-    a.utm_medium,
-    a.utm_campaign,
-    a.visitors_count,
-    ac.total_cost,
-    a.leads_count,
-    a.purchases_count,
-    a.revenue
-from aggregation as a
-left join ad_costs as ac
-    on
-        a.visit_date = ac.campaign_date::date
-        and a.utm_source = ac.utm_source
-        and a.utm_medium = ac.utm_medium
-        and a.utm_campaign = ac.utm_campaign
-order by
-    revenue desc nulls last,
-    visit_date asc,
-    visitors_count desc,
-    utm_source asc, utm_medium asc, utm_campaign asc
+    select
+        a.visit_date,
+        a.utm_source,
+        a.utm_medium,
+        a.utm_campaign,
+        a.visitors_count,
+        ac.total_cost,
+        a.leads_count,
+        a.purchases_count,
+        a.revenue
+    from aggregation as a
+    left join ad_costs as ac
+        on
+            a.visit_date = ac.campaign_date::date
+            and a.utm_source = ac.utm_source
+            and a.utm_medium = ac.utm_medium
+            and a.utm_campaign = ac.utm_campaign
+    order by
+        a.revenue desc nulls last,
+        a.isit_date asc,
+        a.visitors_count desc,
+        a.utm_source asc, utm_medium asc, utm_campaign asc
 )
 
 select
@@ -335,8 +335,8 @@ select
     end as roi
 from aggregate_last_paid_click
 where utm_source in ('vk', 'yandex')
-group by 1, 2, 3
-order by 1;
+group by  utm_source, utm_medium, utm_campaign
+order by utm_source;
 
 
 
